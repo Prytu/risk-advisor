bebcc29ea6f4ac9fb271511caa9c4b4bd067f9c4
Jacek Maksymowicz <jacek.maksymowicz@student.uw.edu.pl>
2016-11-27 19:04:47 +0100

    Cosmetic changes in the code

--- risk_advisor_server.go
+++ risk_advisor_server.go
@@ -25,41 +25,45 @@ type AdviceStatus struct {
 }
 
 type AdviceService struct {
-	proxy_url string
-	last_id   int
+	proxyUrl string
+	lastId   int
 }
 
 func (a *AdviceService) sendAdviceRequest(request *restful.Request, response *restful.Response) {
 	//TODO: do this atomically
-	this_id := a.last_id
-	a.last_id++
+	thisId := a.lastId
+	a.lastId++
 
 	pod := new(api.Pod)
 	err := request.ReadEntity(&pod)
 	if err == nil {
-		ar := AdviceRequest{this_id, *pod}
-		ar_json, _ := json.Marshal(ar)
+		ar := AdviceRequest{thisId, *pod}
+		ar_json, err := json.Marshal(ar)
+		if err != nil {
+			response.WriteError(http.StatusInternalServerError, err)
+			return
+		}
 
-		resp, err := http.Post(a.proxy_url, "application/json", bytes.NewReader(ar_json))
+		resp, err := http.Post(a.proxyUrl, "application/json", bytes.NewReader(ar_json))
 		if err != nil {
 			response.WriteError(http.StatusNotFound, err)
 			return
 		}
 
-		binding := api.Binding{}
 		binding_json, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			response.WriteError(http.StatusInternalServerError, err)
 			return
 		}
 
+		binding := api.Binding{}
 		err = json.Unmarshal(binding_json, &binding)
 		if err != nil {
 			response.WriteError(http.StatusExpectationFailed, err)
 			return
 		}
 
-		status := AdviceStatus{this_id, "OK", binding}
+		status := AdviceStatus{thisId, "OK", binding}
 		response.WriteEntity(status)
 	} else {
 		response.WriteError(http.StatusInternalServerError, err)
@@ -68,13 +72,12 @@ func (a *AdviceService) sendAdviceRequest(request *restful.Request, response *re
 
 func main() {
 	var port *int = flag.Int("port", 11111, "Port to listen on")
-	var base_url *string = flag.String("proxy-url", "http://localhost:9998/", "URL of proxy server")
+	var proxyUrl *string = flag.String("proxy-url", "http://localhost:9998/", "URL of proxy server")
 	flag.Parse()
 
-	as := AdviceService{proxy_url: *base_url, last_id: 1}
+	as := AdviceService{proxyUrl: *proxyUrl, lastId: 1}
 	aws := new(restful.WebService)
-	aws.
-		Path("/advice").
+	aws.Path("/advice").
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
